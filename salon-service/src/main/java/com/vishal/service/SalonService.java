73975b35ab14fb4436863fd270aaa067f56af8e5
VishalMandala <vishalrao.mandala@gmail.com>
2025-10-04 23:50:14 -0400

    Update SalonService for salon microservice

--- salon-service/src/main/java/com/vishal/service/SalonService.java
+++ salon-service/src/main/java/com/vishal/service/SalonService.java
@@ -4,8 +4,22 @@ import com.vishal.model.Salon;
 import com.vishal.payload.dto.SalonDTO;
 import com.vishal.payload.dto.UserDTO;
 
+import java.util.List;
+
 public interface SalonService {
 
     Salon createSalon(SalonDTO salon, UserDTO user);
+
+    Salon updateSalon(SalonDTO salon, UserDTO user, Long SalonId) throws Exception;
+
+    List<Salon> getAllSalons();
+
+    Salon getSalonById(Long id) throws Exception;
+
+    Salon getSalonByOwnerId(String ownerId);
+
+    List<Salon> getSalonsByCity(String city);
+
+    void deleteSalonById(Long id);
 
 }

7756b3bc66a11ec9922d9ad571c8e57dd7aa3c09
VishalMandala <vishalrao.mandala@gmail.com>
2025-10-08 19:02:52 -0400

    Created SalonDTO.java for Category microservice

---
+++ category-service/src/main/java/com/vishal/dto/SalonDTO.java
@@ -0,0 +1,30 @@
+package com.vishal.dto;
+
+import lombok.Data;
+
+import java.time.LocalTime;
+import java.util.List;
+
+@Data
+public class SalonDTO {
+
+    private Long id;
+
+    private String name;
+
+    private List<String> images;
+
+    private String address;
+
+    private String mobile;
+
+    private String email;
+
+    private String city;
+
+    private Long ownerId;
+
+    private LocalTime openedTime;
+
+    private LocalTime closedTime;
+}

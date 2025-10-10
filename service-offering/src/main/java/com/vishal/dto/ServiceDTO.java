b7ad089a99b91c38b845f16e5c2771d7f2dc6e81
VishalMandala <vishalrao.mandala@gmail.com>
2025-10-10 19:45:06 -0400

    Created ServiceDTO.java for service offering microservice

---
+++ service-offering/src/main/java/com/vishal/dto/ServiceDTO.java
@@ -0,0 +1,23 @@
+package com.vishal.dto;
+
+import lombok.Data;
+
+@Data
+public class ServiceDTO {
+
+    private Long id;
+
+    private String name;
+
+    private String description;
+
+    private int price;
+
+    private Long salonId;
+
+    private int duration;
+
+    private Long category;
+
+    private String image;
+}

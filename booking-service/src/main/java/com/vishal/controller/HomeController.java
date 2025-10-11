c4055ac73fd108b3e13914a7be857328ab3cc0da
VishalMandala <vishalrao.mandala@gmail.com>
2025-10-11 19:53:27 -0400

    Created HomeController.java in Booking microservice

---
+++ booking-service/src/main/java/com/vishal/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.vishal.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping
+    public String HomeControllerHandler(){
+        return "Booking microservice for Salon appointment booking system";
+    }
+}

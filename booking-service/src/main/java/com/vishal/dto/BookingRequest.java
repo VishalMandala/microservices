b7ac2eae2ecfc3f1404089f9221e02bdaa556bb7
VishalMandala <vishalrao.mandala@gmail.com>
2025-10-11 19:56:15 -0400

    Created BookingRequest.java in Booking microservice

---
+++ booking-service/src/main/java/com/vishal/dto/BookingRequest.java
@@ -0,0 +1,15 @@
+package com.vishal.dto;
+
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Set;
+
+@Data
+public class BookingRequest {
+
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
+    private Set<Long> serviceIds;
+}

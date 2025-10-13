cd9f75ff592caaa8ccf50fdf0ee5bbeed7d854e2
VishalMandala <vishalrao.mandala@gmail.com>
2025-10-12 22:12:29 -0400

    Created PaymentOrderRepository.java repository in Payment microservice

---
+++ payment-service/src/main/java/com/vishal/repository/PaymentOrderRepository.java
@@ -0,0 +1,9 @@
+package com.vishal.repository;
+
+import com.vishal.model.PaymentOrder;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PaymentOrderRepository extends JpaRepository<PaymentOrder, Long> {
+    PaymentOrder findByPaymentLinkId(String paymentLinkId);
+
+}

0b63f38e26f94261f043d7155a7ffcb38cec184d
VishalMandala <vishalrao.mandala@gmail.com>
2025-10-21 20:24:06 -0400

    Created a new SignupDTO.java DTO in KeycloakService

---
+++ user-service/src/main/java/com/vishal/payload/dto/SignupDTO.java
@@ -0,0 +1,13 @@
+package com.vishal.payload.dto;
+
+import lombok.Data;
+
+@Data
+public class SignupDTO {
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private String username;
+}
